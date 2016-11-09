e0534c626c4c182ce499681cb9c8750f042424b1
陈颜俊 <inter.chen@gmail.com>
2016-11-09 18:11:12 +0800

    fix build issue

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
 	name: "MySQLStORM",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 0),
+		.Package(url: "https://github.com/interchen/Perfect-MySQL.git", majorVersion: 2, minor: 0),
 		.Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 0, minor: 0)
 	],
 	exclude: []
